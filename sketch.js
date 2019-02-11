dcb56b3d58a7328c76841afdb6707b2872840563
Gabriel Urbani <47043330+gabrielurbani@users.noreply.github.com>
2019-02-11 19:36:09 -0300

    zoom 2

--- sketch.js
+++ sketch.js
@@ -10,11 +10,11 @@ let clon = 0;
 let lat = -34.6083;
 let lon = -58.3712
 
-let zoom = 3;
+let zoom = 2;
 let earthquakes;
 
 function preload(){
- mapImg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,3,0/1024x512?access_token=pk.eyJ1Ijoibm9tYWRlcSIsImEiOiJjanJ6MHBtbXEwamczNDlvYWxqc2UzM2dzIn0.MrP0H6Id5uaXMzajMrwkxg");
+ mapImg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,2,0/1024x512?access_token=pk.eyJ1Ijoibm9tYWRlcSIsImEiOiJjanJ6MHBtbXEwamczNDlvYWxqc2UzM2dzIn0.MrP0H6Id5uaXMzajMrwkxg");
  //earthquakes = loadString(
 };
 
