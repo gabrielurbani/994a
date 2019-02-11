15a6b2955c6e4446442e0848eec7c6f5c0ad0496
Gabriel Urbani <47043330+gabrielurbani@users.noreply.github.com>
2019-02-10 23:09:19 -0300

    dark-v9

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let mapImg;
 function preload(){
-  mapImg = loadImage("https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/0,0,1,0/1024x512?access_token=pk.eyJ1Ijoibm9tYWRlcSIsImEiOiJjanJ6MHBtbXEwamczNDlvYWxqc2UzM2dzIn0.MrP0H6Id5uaXMzajMrwkxg");
+  mapImg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,1,0/1024x512?access_token=pk.eyJ1Ijoibm9tYWRlcSIsImEiOiJjanJ6MHBtbXEwamczNDlvYWxqc2UzM2dzIn0.MrP0H6Id5uaXMzajMrwkxg");
 }
 
 function setup(){
